5b461ad31ac727657dcc9b30a0400ad1ba1e09f7
Priyanka3020 <76874796+Priyanka3020@users.noreply.github.com>
2023-07-28 00:12:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
+
+const location_not_found = document.querySelector('.location-not-found');
+
+const weather_body = document.querySelector('.weather-body');
+async function checkWeather(city){
+
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '004789ef27msh1d7a67077cfd6b2p1b3ea3jsn88b89a665a7c',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
+temperature.innerHTML = `${Math.round(result.main.temp - 273.15)}°C`;
+
+    
+
+}
+
+
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value);
+});
